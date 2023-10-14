5e7c2a613ff29d21d17d51e8eed8ef1c8b2d884f
poemaman <luis.campos.pousa@gmail.com>
2023-10-14 08:37:57 +0200

    more s2

---
+++ S2/3.loops/exercise-4.js
@@ -0,0 +1,10 @@
+const toys = [
+{id: 5, name: 'Buzz MyYear'}, 
+{id: 11, name: 'Action Woman'}, 
+{id: 23, name: 'Barbie Man'}, 
+{id: 40, name: 'El gato con Guantes'},
+{id: 40, name: 'El gato felix'}];
+for(const [key, value] of Object.entries(toys)) {if (Object.entries(toys).includes('gato'))
+    {toys.splice(i,1)};
+};
+console.log(toys);
